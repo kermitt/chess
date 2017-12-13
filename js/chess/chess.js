86367d55631a43b04338db5512020c45eb66b1fd
Montgomery <r621362@TRG519305.domain>
2017-12-12 17:27:47 -0800

    starting to look like a chess board

--- js/chess/chess.js
+++ js/chess/chess.js
@@ -1,107 +1,126 @@
-    var drag = d3.behavior.drag()
-        .on('drag', function (d, i) {
-          d.x += d3.event.dx
-          d.y += d3.event.dy
-          d3.select(this).attr('transform', function (d, i) {
-            return 'translate(' + [ d.x, d.y ] + ')'
-          })
+let drag = d3.behavior.drag()
+    .on('drag', function (d, i) {
+      d.x += d3.event.dx
+      d.y += d3.event.dy
+      d3.select(this).attr('transform', function (d, i) {
+        return 'translate(' + [d.x, d.y] + ')'
+      })
+    })
+
+let w = 800
+let h = 800
+let size = w / 9
+d3.select('body')
+    .append('svg:svg')
+    .attr('width', w)
+    .attr('height', h)
+    .attr('id', 'charts')
+    // .on("click", clickypie)
+    .append('svg:rect')
+    .attr('width', '100%')
+    .attr('height', '100%')
+    .attr('stroke', '#000')
+    .attr('stroke-width', 3)
+    .attr('fill', 'none')
+
+function doSomething () {
+  d3.select('#id6').attr('class', 'yay').attr('height', 2)
+}
+
+function populate () {
+  // the letters and numbers bordering the actual board
+  d3.json('js/chess/border.json', function (LoH) {
+    LoH.forEach((cell) => {
+      cell.x = cell.col * size
+      cell.y = cell.row * size
+
+      addCell(cell)
+    })
+  })
+  let pieces = []
+  // the actual playing board
+  d3.json('js/chess/setup.json', function (LoH) {
+    LoH.forEach((cell, i) => {
+      cell.x = cell.col * size
+      cell.y = cell.row * size
+      cell.cx = cell.x + (size / 2)
+      cell.cy = cell.y + (size / 2)
+
+      addCell(cell)
+      if (cell.piece.length > 0) {
+        // addPiece(cell.piece, cell.cx, cell.cy)
+        pieces.push(cell)
+      }
+      // Ok, finish w/ the cells. Now add the pieces...   ...I'm
+      // not using any z-index controls, so add the pieces last so they are
+      // 'on top' on each cell
+      if (i == 63) {
+        pieces.forEach((cell) => {
+          addPiece(cell.piece, cell.cx, cell.cy)
         })
+      }
+    })
+  })
+}
+populate()
 
-    function piece_factory (classname, x, y, r) {
-      var face = d3.select('#charts')
-            .append('svg:g')
-                .data([ {'x': x, 'y': y} ]) // needed for dragging
-                .attr('transform', 'translate(' + x + ',' + y + ')')
-                .call(drag)
+function addPiece (pieceName, x, y) {
+  console.log('pieceName: ' + pieceName + ' x ' + x + ' +    y ' + y)
+  let r = size / 2
+  let face = d3.select('#charts')
+        .append('svg:g')
+        .data([{
+          'x': x,
+          'y': y
+        }]) // needed for dragging
+        .attr('transform', 'translate(' + x + ',' + y + ')')
+        .call(drag)
 
-      var head = face.append('svg:circle')
-                .attr('class', 'cssthing')
-                .attr('fill', function (d) { return '#e0e0e0' })
-                .attr('fill-opacity', 0.8)
-                .attr('stroke', '#000')
-                .attr('stroke-width', 4)
-                .attr('r', r)
+  let head = face.append('svg:circle')
+        .attr('class', 'cssthing')
+        .attr('fill', function (d) {
+          return '#e0e0e0'
+        })
+        .attr('fill-opacity', 0.8)
+        .attr('stroke', '#000')
+        .attr('stroke-width', 4)
+        .attr('r', r)
 
-      var text = face.append('svg:text')
-            .text(BLACK_KING.unicode)
-            .attr('y', '.1em')
-            .style('font-size', 40)
-            .attr('transform', 'translate(' + [0, r / 3] + ')')
-            .attr('text-anchor', 'middle')
-            .attr('font-weight', 700)
-            .attr('font-family', 'Helvetica')
-            .attr('fill', '#000')
-            .attr('stroke', 'none')
-            .attr('pointer-events', 'none')
-    }
+  let text = face.append('svg:text')
+        .text(pieces[pieceName].unicode)
+        .attr('y', '.1em')
+        .style('font-size', 40)
+        .attr('transform', 'translate(' + [0, r / 3] + ')')
+        .attr('text-anchor', 'middle')
+        .attr('font-weight', 700)
+        .attr('font-family', 'Helvetica')
+        .attr('fill', '#000')
+        .attr('stroke', 'none')
+        .attr('pointer-events', 'none')
+}
 
-/// ////////////////////////
+function addCell (obj) {
+  let x = obj.x// col * size
+  let y = obj.y// row * size
 
-    function cell_factory (classname, x, y, width, height, id) {
-      var face = d3.select('#charts')
-            .append('svg:g')
-                .attr('transform', 'translate(' + x + ',' + y + ')')
+  let face = d3.select('#charts')
+        .append('svg:g')
+        .attr('transform', 'translate(' + x + ',' + y + ')')
+  let head = face.append('svg:rect')
+        .attr('id', obj.id)
+        .attr('fill', obj.color)
+        .attr('fill-opacity', 1.0)
+        .attr('stroke-width', 4)
+        .attr('width', size)
+        .attr('height', size)
 
-      var head = face.append('svg:rect')
-  .attr('class', 'cssthing')
-                .attr('id', id)
-
-                              .attr('fill', function (d) { return '#e0e0e0' })
-                .attr('fill-opacity', 0.8)
-                .attr('stroke', '#000')
-                .attr('stroke-width', 4)
-                .attr('width', width)
-                .attr('height', height)
-
-      var text = face.append('svg:text')
-            .text(id)
-             .attr("y", ".5em")
-            // .attr("transform", "translate(" + [0, 10] + ")")
-            .attr('text-anchor', 'middle')
-            .attr('font-weight', 700)
-            .attr('font-family', 'Helvetica')
-            .attr('fill', '#000')
-            .attr('stroke', 'none')
-            .attr('pointer-events', 'none')
-    }
-
-/// /////////////////////////
-    var w = 800
-    var h = 800
-    var size = 50
-
-    // setup svg canvas
-    d3.select('body')
-        .append('svg:svg')
-            .attr('width', w)
-            .attr('height', h)
-            .attr('id', 'charts')
-            // .on("click", clickypie)
-            .append('svg:rect')
-                .attr('width', '100%')
-                .attr('height', '100%')
-                .attr('stroke', '#000')
-                .attr('stroke-width', 3)
-                .attr('fill', 'none')
-
-    for (i = 0; i < 10; i++) {
-      let x = Math.random() * w
-      let y = Math.random() * h
-      let width = size
-      let height = size
-      id = 'id' + i
-      cell_factory('cell' + i, x, y, width, height, id)
-    }
-
-  // r = 100;
-    for (i = 0; i < 3; i++) {
-      let r = size / 2
-
-      let x = Math.random() * w
-      let y = Math.random() * h
-      piece_factory('face' + i, x, y, r)
-    }
-
-    function doSomething () {
-      d3.select('#id6').attr('class', 'yay').attr("height",2)
-    }
+  let text = face.append('svg:text')
+        .text(obj.i)
+        .attr('transform', 'translate(' + [(size - 10) / 2, (size + 12) / 2] + ')')
+        .attr('text-anchor', 'right')
+        .attr('font-weight', 700)
+        .attr('font-family', 'Helvetica')
+        .attr('fill', '#000')
+        .attr('stroke', 'none')
+        .attr('pointer-events', 'none')
+}
--- js/chess/pieces.js
+++ js/chess/pieces.js
@@ -1,22 +1,22 @@
+const pieces = {} 
 class Piece {
   constructor (name, unicode) {
-    // e.g., 'white king', '\u2654'
+    // e["g[", 'white king', '\u2654'
     this.name = name
     this.unicode = unicode
     this.current_row = 0
     this.current_col = 0
   }
 }
-
-let WHITE_KING = new Piece('White King', '\u2654')
-let BLACK_KING = new Piece('Black King', '\u265A')
-let WHITE_QUEEN = new Piece('White Queen', '\u2655')
-let BLACK_QUEEN = new Piece('Black Queen', '\u265B')
-let WHITE_ROOK = new Piece('White Rook', '\u2656')
-let BLACK_ROOK = new Piece('Black Rook', '\u265C')
-let WHITE_BISHOP = new Piece('White Bishop', '\u2657')
-let BLACK_BISHOP = new Piece('Black Bishop', '\u265D')
-let WHITE_KNIGHT = new Piece('White Knight', '\u2658')
-let BLACK_KNIGHT = new Piece('Black Knight', '\u265E')
-let WHITE_PAWN = new Piece('White Pawn', '\u2659')
-let BLACK_PAWN = new Piece('Black Pawn', '\u265F')
+pieces["WHITE_KING"] = new Piece('White King', '\u2654')
+pieces["BLACK_KING"] = new Piece('Black King', '\u265A')
+pieces["WHITE_QUEEN"] = new Piece('White Queen', '\u2655')
+pieces["BLACK_QUEEN"] = new Piece('Black Queen', '\u265B')
+pieces["WHITE_ROOK"] = new Piece('White Rook', '\u2656')
+pieces["BLACK_ROOK"] = new Piece('Black Rook', '\u265C')
+pieces["WHITE_BISHOP"] = new Piece('White Bishop', '\u2657')
+pieces["BLACK_BISHOP"] = new Piece('Black Bishop', '\u265D')
+pieces["WHITE_KNIGHT"] = new Piece('White Knight', '\u2658')
+pieces["BLACK_KNIGHT"] = new Piece('Black Knight', '\u265E')
+pieces["WHITE_PAWN"] = new Piece('White Pawn', '\u2659')
+pieces["BLACK_PAWN"] = new Piece('Black Pawn', '\u265F')
