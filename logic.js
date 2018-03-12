7273bd533312d3dac8307365d26d3932a5c59d36
Montgomery <r621362@ut0101ds11-internal.regence.com>
2018-03-12 08:18:39 -0700

    Getting there...

--- logic.js
+++ logic.js
@@ -1,25 +1,110 @@
-function touch(id) {
-    for ( let key in board ) {
-        let domId = board[key].id
-        document.getElementById(domId).classList.remove("possible");
-    }
-    let p = pieces[id]
-    p.moves.forEach((xy,j)=>{
-        let col = p.column
-        let row = p.row
-        let i = 0
-        let keepAlive = true
-        while ( i < p.travel && keepAlive) {
-            i++
-                col += xy[0]
-                row += xy[1]
-                id = "c" + col + "r" + row
+const BLACK_CELL = 'cell_black'
+const WHITE_CELL = 'cell_white'
+const SUPPORT = "#0000ff"
+const ATTACK = "#ff0000"
+const INFLUENCE = "#ffff00"
+const SELECTED = "#00ffff"
+let possible = {}
+let activePid = ""
 
-                if ( col > -1 && col < 8 && row > -1 && row < 8 ) {
-                        let domId = board[id].id
-                    document.getElementById(domId).classList.add("possible");
-                } else {
-                }
+const getRowCol= (candidate) => {
+  //"r2c6"
+  let ary = candidate.split("c")
+  let row = parseInt(ary[0].replace("r",""))
+  let col = parseInt(ary[1])
+  return [row,col]
+}
+const resetAllCells = () => {
+  for ( var id in board) {
+    resetCellColor(id)
+  }
+}
+const resetCellColor = (id) => {
+  let bgcolor = "#00ff00"
+  if ( board[id].css == WHITE_CELL) {
+    bgcolor="#ffffff"
+  }
+  document.getElementById(id).style.backgroundColor = bgcolor
+}
+
+const showInfluence = () => {
+  let p = pieces[activePid]
+  p.moves.forEach((xy)=>{
+    let x = p.x
+    let y = p.y
+    let i = 0
+    let continueOn = true
+    while ( i < p.travel && continueOn) {
+      i++
+      x += xy[0]
+      y += xy[1]
+      if ( x > -1 && x < 8 && y > -1 && y < 8 ) {
+        let cellId = 'r' + x + 'c' + y
+        let pid = board[cellId].pid
+
+        if ( pid.length > 0) {
+          let b = pieces[pid]
+          let a = pieces[activePid]
+          if ( a.color === b.color ) {
+            possible[cellId] = SUPPORT
+            continueOn = false
+          } else {
+            possible[cellId] = ATTACK
+            continueOn = false
+          }
+        } else {
+          possible[cellId] = INFLUENCE
         }
-    })
+      }
+      for ( let cellId in possible ) { 
+        document.getElementById(cellId).style.backgroundColor = possible[cellId]
+      }
+    }
+  })
+}
+
+
+function cell_click (human, cellId) {
+  let pid = board[cellId].pid
+  if ( pid.length > 0 && ! possible.hasOwnProperty(cellId)) {
+    possible = {}
+    resetAllCells()
+    activePid = pid
+    showInfluence()
+    document.getElementById(cellId).style.backgroundColor = SELECTED
+    let piece = pieces[pid]
+  } else {
+    if ( possible.hasOwnProperty(cellId)) {
+      if ( possible[cellId] == INFLUENCE ) {
+        let a = pieces[activePid]
+        document.getElementById(a.boardId).innerHTML = ""
+        board[a.boardId].pid = ""
+        a.boardId = cellId
+        board[a.boardId].pid = a.id
+        document.getElementById(a.boardId).innerHTML = a.html
+        let xy = getRowCol(cellId)
+        a.x = xy[0]
+        a.y = xy[1]
+        possible = {}
+        resetAllCells()
+      } 
+      else if ( possible[cellId] == ATTACK ) {
+console.log("ATTACK !")
+          let a = pieces[activePid]
+          document.getElementById(a.boardId).innerHTML = ""
+          board[a.boardId].pid = ""
+          a.boardId = cellId
+          board[a.boardId].pid = a.id
+
+          document.getElementById(a.boardId).innerHTML = a.html
+          let xy = getRowCol(cellId)
+          a.x = xy[0]
+          a.y = xy[1]
+          possible = {}
+          resetAllCells()
+      }
+    } else {
+    }
+    console.log("! pid >" + pid + "<   " + activePid )
+  }
 }
--- pieces.js
+++ pieces.js
@@ -1,38 +1,28 @@
-const WHITE = "white"
-const BLACK = "black"
+const WHITE = 'white'
+const BLACK = 'black'
 class Piece {
-    constructor (unicode, row, column, color, id, name, travel) {
-      this.unicode = unicode // diplay
-      this.column = column // col
-      this.row = row // row
-    
-      this.color = color // side
-      this.name = name // name
-      this.moveCount = 0 // this will be important for Castling and En-passant... and, hopefully, for analytics
-      this.travel = travel// # possible spaces - this will get weird for pawns
-      this.id = id
+  constructor (unicode, row, column, color, id, name, travel) {
+    this.unicode = unicode // diplay
+    this.y = column
+    this.x = row
+    this.boardId = 'r' + this.x + 'c' + this.y
+    this.color = color // side
+    this.moveCount = 0 // this will be important for Castling and En-passant... and, hopefully, for analytics
+    this.travel = travel// # possible spaces - this will get weird for pawns
+    this.id = id
+    this.html = "<div class='piece'>" + this.unicode + '</div>'
+    board[this.boardId].pid = id
+    document.getElementById(this.boardId).innerHTML = this.html
+  }
+  getTravel () {
+    return this.travel
+  }
+}
 
-      let item = "<div class='piece' onclick='touch(\"" + this.id + "\")'>" + this.unicode + "</div>"
-      document.getElementById(this.getDomId()).innerHTML = item   
-    }
-    getColRow_currentCell () {
-      return {c: this.column, r: this.row, cellId: getCellId_fromColumnAndRow(this.column, this.row), pieceId: this.id}
-    }
-    getTravel () {
-      return this.travel
-    }
-    getDomId() {
-        // convert something like 'c0r0' into 'a1'
-        let rc = "c" + this.column + "r" + this.row
-        //return lookup[rc]
-        return board[rc].id
-    }
-  }
-  
-  class King extends Piece {
-    constructor (unicode, column, row, color, id, name) {
-      super(unicode, column, row, color, id, name, 1)
-      this.moves = [
+class King extends Piece {
+  constructor (unicode,  y,x, color, id, name) {
+    super(unicode, y,x, color, id, name, 1)
+    this.moves = [
       [-1, -1],
       [1, -1],
       [1, 1],
@@ -41,13 +31,13 @@ class Piece {
       [0, -1],
       [1, 0],
       [-1, 0]
-      ]
-    }
+    ]
   }
-  class Queen extends Piece {
-    constructor (unicode, column, row, color, id, name) {
-      super(unicode, column, row, color, id, name, 7)
-      this.moves = [
+  }
+class Queen extends Piece {
+  constructor (unicode, y,x, color, id, name) {
+    super(unicode,  y,x, color, id, name, 7)
+    this.moves = [
       [-1, -1],
       [1, -1],
       [1, 1],
@@ -56,36 +46,36 @@ class Piece {
       [0, -1],
       [1, 0],
       [-1, 0]
-      ]
-    }
+    ]
   }
-  class Rook extends Piece {
-    constructor (unicode, column, row, color, id, name) {
-      super(unicode, column, row, color, id, name, 7)
-      this.moves = [
+  }
+class Rook extends Piece {
+  constructor (unicode, y,x, color, id, name) {
+    super(unicode, y,x, color, id, name, 7)
+    this.moves = [
         [0, 1],
         [0, -1],
         [1, 0],
         [-1, 0]
-        ]    
-    }
+    ]
   }
-  
-  class Bishop extends Piece {
-    constructor (unicode, column, row, color, id, name) {
-      super(unicode, column, row, color, id, name, 7)
-      this.moves = [
+  }
+
+class Bishop extends Piece {
+  constructor (unicode, y,x, color, id, name) {
+    super(unicode,  y,x, color, id, name, 7)
+    this.moves = [
         [-1, -1],
         [1, -1],
         [1, 1],
         [-1, 1]
-        ]
-    }
+    ]
   }
-  class Knight extends Piece {
-    constructor (unicode, column, row, color, id, name) {
-      super(unicode, column, row, color, id, name, 1)
-      this.moves = [
+  }
+class Knight extends Piece {
+  constructor (unicode, y,x, color, id, name) {
+    super(unicode, y,x, color, id, name, 1)
+    this.moves = [
       [-1, -2],
       [1, -2],
       [2, -1],
@@ -94,76 +84,75 @@ class Piece {
       [-1, 2],
       [-2, 1],
       [-2, -1]
-      ]
-    }
+    ]
+  }
   }
 
-  class PawnWhite extends Piece {
-    constructor (unicode, column, row, color, id, name) {
-      super(unicode, column, row, color, id, name, 1)
-      this.moves = [
-      [-1,0],
-      ]
-    }
+class PawnWhite extends Piece {
+  constructor (unicode, y,x, color, id, name) {
+    super(unicode, y,x, color, id, name, 1)
+    this.moves = [
+      [-1, 0]
+    ]
   }
-  class PawnBlack extends Piece {
-    constructor (unicode, column, row, color, id, name) {
-      super(unicode, column, row, color, id, name, 1)
-      this.moves = [
-        [1,0],
-        ]
-    }
+  }
+class PawnBlack extends Piece {
+  constructor (unicode, y,x, color, id, name) {
+    super(unicode, y,x, color, id, name, 1)
+    this.moves = [
+        [1, 0]
+    ]
+  }
   }
 
-  
   // + --------------------------------------------------------------------------+
-  let pieces = {}
-  
-  pieces['wk'] = new King('\u2654', 4, 7, WHITE, 'wk', 'white_king')
-  pieces['bk'] = new King('\u265A', 4, 0, BLACK, 'bk', 'black_king')
+let pieces = {}
+
+pieces['wk'] = new King('\u2654', 7,4, WHITE, 'wk', 'white_king')
+pieces['bk'] = new King('\u265A', 0,4, BLACK, 'bk', 'black_king')
       //
-  pieces['wq'] = new Queen('\u2655', 3, 7, WHITE, 'wq', 'white_queen')
-  pieces['bq'] = new Queen('\u265B', 3, 0, BLACK, 'bq', 'black_queen')
+pieces['wq'] = new Queen('\u2655', 7,3, WHITE, 'wq', 'white_queen')
+pieces['bq'] = new Queen('\u265B', 0,3, BLACK, 'bq', 'black_queen')
       //
-  pieces['wr1'] = new Rook('\u2656', 0, 7, WHITE, 'wr1', 'white_rook_queenside')
-  pieces['wr2'] = new Rook('\u2656', 7, 7, WHITE, 'wr2', 'white_rook_kingside')
-  pieces['br1'] = new Rook('\u265C', 0, 0, BLACK, 'br1', 'black_rook_queenside')
-  pieces['br2'] = new Rook('\u265C', 7, 0, BLACK, 'br2', 'black_rook_kingside')
+pieces['wr1'] = new Rook('\u2656', 7,0, WHITE, 'wr1', 'white_rook_queenside')
+pieces['wr2'] = new Rook('\u2656', 7, 7, WHITE, 'wr2', 'white_rook_kingside')
+pieces['br1'] = new Rook('\u265C', 0, 0, BLACK, 'br1', 'black_rook_queenside')
+pieces['br2'] = new Rook('\u265C', 0,7, BLACK, 'br2', 'black_rook_kingside')
       //
-  pieces['wb1'] = new Bishop('\u2657', 2, 7, WHITE, 'wb1', 'white_bishop_queenside')
-  pieces['wb2'] = new Bishop('\u2657', 5, 7, WHITE, 'wb2', 'white_bishop_kingside')
-  pieces['bb1'] = new Bishop('\u265D', 2, 0, BLACK, 'bb1', 'black_bishop_queenside')
-  pieces['bb2'] = new Bishop('\u265D', 5, 0, BLACK, 'bb2', 'black_bishop_kingside')
+pieces['wb1'] = new Bishop('\u2657', 7,2, WHITE, 'wb1', 'white_bishop_queenside')
+pieces['wb2'] = new Bishop('\u2657', 7,5, WHITE, 'wb2', 'white_bishop_kingside')
+pieces['bb1'] = new Bishop('\u265D', 0,2, BLACK, 'bb1', 'black_bishop_queenside')
+pieces['bb2'] = new Bishop('\u265D', 0,5, BLACK, 'bb2', 'black_bishop_kingside')
       //
-  pieces['wk1'] = new Knight('\u2658', 1, 7, WHITE, 'wk1', 'white_knight_queenside')
-  pieces['wk2'] = new Knight('\u2658', 6, 7, WHITE, 'wk2', 'white_knight_kingside')
-  pieces['bk1'] = new Knight('\u265E', 1, 0, BLACK, 'bk1', 'black_knight_queenside')
-  pieces['bk2'] = new Knight('\u265E', 6, 0, BLACK, 'bk2', 'black_knight_kingside')
+pieces['wk1'] = new Knight('\u2658', 7,1, WHITE, 'wk1', 'white_knight_queenside')
+pieces['wk2'] = new Knight('\u2658', 7,6, WHITE, 'wk2', 'white_knight_kingside')
+pieces['bk1'] = new Knight('\u265E', 0,1, BLACK, 'bk1', 'black_knight_queenside')
+pieces['bk2'] = new Knight('\u265E', 0,6, BLACK, 'bk2', 'black_knight_kingside')
       //
-  pieces['wp1'] = new PawnWhite('\u2659', 0, 6, WHITE, 'wp1', 'white_pawn_1')
-  pieces['wp2'] = new PawnWhite('\u2659', 1, 6, WHITE, 'wp2', 'white_pawn_2')
-  pieces['wp3'] = new PawnWhite('\u2659', 2, 6, WHITE, 'wp3', 'white_pawn_3')
-  pieces['wp4'] = new PawnWhite('\u2659', 3, 6, WHITE, 'wp4', 'white_pawn_4')
-  pieces['wp5'] = new PawnWhite('\u2659', 4, 6, WHITE, 'wp5', 'white_pawn_5')
-  pieces['wp6'] = new PawnWhite('\u2659', 5, 6, WHITE, 'wp6', 'white_pawn_6')
-  pieces['wp7'] = new PawnWhite('\u2659', 6, 6, WHITE, 'wp7', 'white_pawn_7')
-  pieces['wp8'] = new PawnWhite('\u2659', 7, 6, WHITE, 'wp8', 'white_pawn_8')
+pieces['wp1'] = new PawnWhite('\u2659', 6,0,  WHITE, 'wp1', 'white_pawn_1')
+pieces['wp2'] = new PawnWhite('\u2659', 6,1,  WHITE, 'wp2', 'white_pawn_2')
+pieces['wp3'] = new PawnWhite('\u2659', 6,2,  WHITE, 'wp3', 'white_pawn_3')
+pieces['wp4'] = new PawnWhite('\u2659', 6,3,  WHITE, 'wp4', 'white_pawn_4')
+pieces['wp5'] = new PawnWhite('\u2659', 6,4,  WHITE, 'wp5', 'white_pawn_5')
+pieces['wp6'] = new PawnWhite('\u2659',6, 5,  WHITE, 'wp6', 'white_pawn_6')
+pieces['wp7'] = new PawnWhite('\u2659', 6,6,  WHITE, 'wp7', 'white_pawn_7')
+pieces['wp8'] = new PawnWhite('\u2659', 6,7,  WHITE, 'wp8', 'white_pawn_8')
       //
-  pieces['bp1'] = new PawnBlack('\u265F', 0, 1, BLACK, 'bp1', 'black_pawn_1')
-  pieces['bp2'] = new PawnBlack('\u265F', 1, 1, BLACK, 'bp2', 'black_pawn_2')
-  pieces['bp3'] = new PawnBlack('\u265F', 2, 1, BLACK, 'bp3', 'black_pawn_3')
-  pieces['bp4'] = new PawnBlack('\u265F', 3, 1, BLACK, 'bp4', 'black_pawn_4')
-  pieces['bp5'] = new PawnBlack('\u265F', 4, 1, BLACK, 'bp5', 'black_pawn_5')
-  pieces['bp6'] = new PawnBlack('\u265F', 5, 1, BLACK, 'bp6', 'black_pawn_6')
-  pieces['bp7'] = new PawnBlack('\u265F', 6, 1, BLACK, 'bp7', 'black_pawn_7')
-  pieces['bp8'] = new PawnBlack('\u265F', 7, 1, BLACK, 'bp8', 'black_pawn_8')
-  
-  try {
-    module.exports = {
-      pieces: pieces
-    }
-  } catch (ignore) {
+pieces['bp1'] = new PawnBlack('\u265F', 1,0,  BLACK, 'bp1', 'black_pawn_1')
+pieces['bp2'] = new PawnBlack('\u265F', 1,1,  BLACK, 'bp2', 'black_pawn_2')
+pieces['bp3'] = new PawnBlack('\u265F', 1,2,  BLACK, 'bp3', 'black_pawn_3')
+pieces['bp4'] = new PawnBlack('\u265F',1, 3,  BLACK, 'bp4', 'black_pawn_4')
+pieces['bp5'] = new PawnBlack('\u265F', 1,4,  BLACK, 'bp5', 'black_pawn_5')
+pieces['bp6'] = new PawnBlack('\u265F', 1,5,  BLACK, 'bp6', 'black_pawn_6')
+pieces['bp7'] = new PawnBlack('\u265F',1,6,  BLACK, 'bp7', 'black_pawn_7')
+pieces['bp8'] = new PawnBlack('\u265F', 1,7,  BLACK, 'bp8', 'black_pawn_8')
+
+try {
+  module.exports = {
+    pieces: pieces
+  }
+} catch (ignore) {
     // Export is for testing purposes...
     // If this 'error' occures, then that means that the context is a browser.
     // QED : Not an error
-  }
+}
