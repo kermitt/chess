abaa7c6ff9681a91c5bdc03b91708a7c00c7d1c3
Montgomery <r621362@TRG519305.domain>
2018-03-17 15:16:07 -0700

    adding 'explore mode'

--- board.js
+++ board.js
@@ -1,1 +1,16 @@
 
+
+
+let play_mode = "mode: play"
+let explore_mode = "mode: explore"
+let current_mode = play_mode
+function setMode() {
+    if ( current_mode == play_mode ) {
+        current_mode = explore_mode
+        document.getElementById("remove_selected").style.display = "inline"
+    } else {
+        current_mode = play_mode
+        document.getElementById("remove_selected").style.display = "none"
+    }
+    document.getElementById("mode").innerHTML = current_mode
+}
--- help.js
+++ help.js
@@ -1,5 +1,6 @@
 // Run this to generate the base board...
 // This file is not part of the real game
+// Once I settle on a board.html then I will throw this file away.
 
 const BLACK_CELL = 'cell_black'
 const WHITE_CELL = 'cell_white'
@@ -12,21 +13,19 @@ l('<html>\n<head>')
 l('<link rel="stylesheet" type="text/css" href="style.css">')
 l('</head>')
 l('<body>')
-//l('<hr>')
-l('<div id="turn"></div>')
+//l('<span id="turn"></span><button id="mode" onclick="setMode();">Mode</button>')
+l('<button id="turn"></button><button id="mode" onclick="setMode();">mode: play</button><button class="hide_show" id="remove_selected" onclick="remove_selected();">remove: selected</button>')
+
 l('<table border="1"><tr><td valign="top">')
 
 l('<table border="1">')
 let cell_color_flipflop = 1
 let board = {}
-//let lookup = {}
 
 for (let row = 0; row < 8; row++) {
   l('\n<tr>')
 
 for (let col = 0;col < 8; col++) {
-//  l('\n<tr>')
-//  for (let row = 0; row < 8; row++) {
     let number = 8 - row // flip! bottom row is 1! Not the highest.
     let human = letters[col] + number
     let css = cell_color_flipflop % 2 == 0 ? BLACK_CELL : WHITE_CELL
@@ -35,23 +34,21 @@ for (let col = 0;col < 8; col++) {
     l("<td valign='center'><div id='" + rc + "' class='" + css + "'   onclick='cell_click(\"" + human + '","' + rc + "\")'></div></td>")
     cell_color_flipflop++
     board[rc] = {human:human, pid: '', influenced: {}, css: css, rc:rc}
-//    lookup[human] = rc
   }
   cell_color_flipflop++
   l('</tr>')
 }
-//l('</table></td><td valign="top"><div id="dead">d</div></td><td valign="top"><textarea class="forHumans" id="pgn" rows="30" cols="100"></textarea></td></tr></table>')
 l('</table></td><td valign="top"><div id="dead"></div></td><td valign="top"><div id="pgn"></div></td></tr></table>')
 l('<div id="active_piece"></div>')
 l('<script>')
 l('let board=' + JSON.stringify(board, null, 6))
-//l('let lookup=' + JSON.stringify(lookup, null, 6))
 l('</script>')
-l('<script src="pieces.js"></script>')
-l('<script src="logic.js"></script>')
-l('<script src="pgn.js"></script>')
+l('<script src="board.js"></script>') // stuff
+l('<script src="pieces.js"></script>') // pieces
+l('<script src="logic.js"></script>') // move logic
+l('<script src="pgn.js"></script>') // pgn stuff : TODO = everything pgn
 l('<!-- Pawns and Kings have weird movement, hence breakout to a diff file //-->')
-l('<script src="pawn_move.js"></script>')
-l('<script src="king_move.js"></script>')
+l('<script src="pawn_move.js"></script>') // pawn move logic ( tricky )
+l('<script src="king_move.js"></script>') // king move logic ( tricky )
 l('</body>')
 l('</html>')
